8b5489522ee0ac6791330d9f77fb20e5c9bdfbd7
shivam.shrivastava <shivam.shrivastava@dare2compete.com>
2022-02-20 18:58:59 +0530

    first commit

---
+++ src/lib/my-lib.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { IndexComponent } from './index/index.component';
+
+
+
+@NgModule({
+  declarations: [
+    IndexComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    IndexComponent
+  ]
+})
+export class MyLibModule { }
---
+++ src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of my-lib
+ */
+
+export * from './lib/my-lib.service';
+export * from './lib/index/index.component';
+export * from './lib/my-lib.module';
